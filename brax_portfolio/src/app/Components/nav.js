e46d47c92bce57ce371a059da548431f546a7762
Brax Wong <ghostenjob@gmail.com>
2025-09-13 15:03:37 +0800

    updated the navbar.

--- brax_portfolio/src/app/Components/nav.js
+++ brax_portfolio/src/app/Components/nav.js
@@ -1,31 +1,146 @@
-import {Navbar, NavbarContent, NavbarItem, Link, NavbarMenuToggle, Button, NavbarMenu, NavbarMenuItem} from "@heroui/react";
-import styles from '../../styles/Nav.module.css'
+import React from 'react';
+import { AppBar, Container, Toolbar, Typography, Box, Button, IconButton, Menu, MenuItem, Avatar } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import styles from "../../styles/Nav.module.css";
+
+const pages = ['Home', 'About', 'Work Experience', 'Personal Projects'];
+const redirect = ['/', '/about', '/work_experience', '/personal_projects'];
 
 export default function Nav() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
     return (
-        <Navbar className={styles.container}>
-            <NavbarContent className="hidden sm:flex gap-4" justify="center">
-               <NavbarItem>
-                   <Link className={styles.text} color="foreground" href="/">
-                        Home
-                    </Link>
-               </NavbarItem>
-                <NavbarItem>
-                   <Link className={styles.text} color="foreground" href="/about">
-                       About
-                   </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link className={styles.text} aria-current="page" href="/work_experience">
-                       Work Experience  
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link className={styles.text} color="foreground" href="/personal_projects">
-                        Personal Projects 
-                    </Link>
-                 </NavbarItem>
-            </NavbarContent>
-        </Navbar>
-    )
+        <AppBar position="static" sx={{ backgroundColor: '#d4c9c9cc' }}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'black',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Brax Wong
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{ display: { xs: 'block', md: 'none' } }}
+                        >
+                            {pages.map((page, index) => (
+                                <MenuItem
+                                    key={page}
+                                    onClick={() => {
+                                        window.location.href = redirect[index];
+                                        handleCloseNavMenu();
+                                    }}
+                                    sx={{ my: 2, color: 'black', display: 'block' }}
+                                >
+                                    <Typography textAlign="center">{page}</Typography> 
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Brax Wong
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page, index) => (
+                            <MenuItem
+                                key={page}
+                                onClick={() => { window.location.href = redirect[index]; handleCloseNavMenu(); }}
+                                sx={{ my: 2, color: 'black', display: 'block' }}
+                                className={styles.text}
+                            >
+                                <Typography textAlign="center">{page}</Typography>
+                            </MenuItem>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <IconButton>
+                            <Avatar className={styles.icon} alt="Remy Sharp" src="/favicon.ico" />
+                        </IconButton>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 }
